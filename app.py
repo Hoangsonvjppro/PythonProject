857dc9e40674d71415be36a9050e349841efb210
Hoangsonvjppro <sonlearnit8386@gmail.com>
2025-03-16 18:44:18 +0700

    commit before big update

--- app.py
+++ app.py
@@ -30,6 +30,9 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 
+
+
+
 # Model User
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
