32ecc030a3c48987805846fa3c5e7cbaf75ab463
Hoangsonvjppro <sonlearnit8386@gmail.com>
2025-05-01 01:06:26 +0700

    qua la met moi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from app import create_app
 from app.extensions import socketio, socketio_available
 
@@ -8,11 +10,11 @@ if __name__ == '__main__':
     if socketio_available:
         try:
             print("Starting application with SocketIO...")
-            socketio.run(app, debug=app.config['DEBUG'], host='0.0.0.0', port=5000)
+            socketio.run(app, debug=app.config['DEBUG'], host='0.0.0.0', port=5001)
         except Exception as e:
             print(f"Error starting SocketIO: {e}")
             print("Falling back to regular Flask server")
-            app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=5000)
+            app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=5001)
     else:
         print("SocketIO is not available. Starting regular Flask server...")
-        app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=5000)
+        app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=5001)
